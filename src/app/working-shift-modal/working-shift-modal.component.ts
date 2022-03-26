2703a2118f0615e8a1732ef3fea4b3a9b9983b64
evgeniaBazh <evgeniabazh7@gmail.com>
2022-03-26 16:12:05 +0500

    Функционал модального окна

--- src/app/working-shift-modal/working-shift-modal.component.ts
+++ src/app/working-shift-modal/working-shift-modal.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-working-shift-modal',
   templateUrl: './working-shift-modal.component.html',
-  styleUrls: ['./working-shift-modal.component.scss']
+  styleUrls: ['./working-shift-modal.component.scss'],
 })
-export class WorkingShiftModalComponent implements OnInit {
-
-  constructor() { }
+export class WorkingShiftModalComponent implements OnInit, OnDestroy {
+  constructor() {}
 
   ngOnInit(): void {
+    window.addEventListener('keydown', this.onKeyDown);
   }
 
+  ngOnDestroy(): void {
+    window.removeEventListener('keydown', this.onKeyDown);
+  }
+
+  isModalVisible: boolean = false;
+
+  show(): void {
+    this.isModalVisible = true;
+  }
+
+  hide(): void {
+    this.isModalVisible = false;
+  }
+
+  onKeyDown = (event: KeyboardEvent): void => {
+    if (event.key === 'Escape') {
+      this.hide();
+    }
+  };
 }
--- src/app/working-shift-table/working-shift-table.component.ts
+++ src/app/working-shift-table/working-shift-table.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkingShiftModalComponent } from '../working-shift-modal/working-shift-modal.component';
 
 @Component({
   selector: 'app-working-shift-table',
   templateUrl: './working-shift-table.component.html',
-  styleUrls: ['./working-shift-table.component.scss']
+  styleUrls: ['./working-shift-table.component.scss'],
 })
 export class WorkingShiftTableComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
